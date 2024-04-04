f5ed2d698c2039ba755d90edfefa2ed1c2dd438b
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-04-04 16:25:16 +0300

    style registerPage

--- src/pages/RegisterPage/RegisterPage.styled.jsx
+++ src/pages/RegisterPage/RegisterPage.styled.jsx
@@ -1,8 +1,11 @@
+import { colors } from '../../constats';
 import styled from 'styled-components';
 
 export const WrapRegisterPage = styled.div`
+  max-width: 374px;
+  background-color: ${colors.bgc_first};
   @media only screen and (min-width: 375px) {
-    width: 375px;
+    width: 100%;
     padding: 20px 20px;
   }
 `;
